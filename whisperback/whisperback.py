2812310ba5ac4545d227145c8c76ba7bf33e2c18
amnesia <amnesia@boum.org>
2009-12-26 18:40:23 +0100

    Update version to 0.1

--- whisperback/whisperback.py
+++ whisperback/whisperback.py
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-VERSION = '0.0'
+VERSION = '0.1'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
