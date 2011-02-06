12ba998b0896b2b267e6fd5951667a7697d42293
amnesia <amnesia@boum.org>
2011-02-06 16:40:45 +0100

    Update version to 1.3.1

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.3'
+__version__ = '1.3.1'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
