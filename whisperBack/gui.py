a10f39175e081240aa3ab7355c6c76fb39876553
amnesia <amnesia@boum.org>
2011-09-14 17:38:28 +0200

    Bump version to 1.4.2

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.4.1'
+__version__ = '1.4.2'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
