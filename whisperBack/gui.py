ad928878f94866e7b1783a89caef5fa09a32dc9b
Tails developers <amnesia@boum.org>
2011-11-02 18:01:11 +0100

    Update version to 1.5

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.5~rc1'
+__version__ = '1.5'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
