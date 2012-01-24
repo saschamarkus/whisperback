b41ef317eb42e69fd533e4709c501a814f97daf5
Tails developers <amnesia@boum.org>
2012-01-24 15:24:47 +0100

    Fix link to bug reporting documentation which have moved

--- whisperBack/utils.py
+++ whisperBack/utils.py
@@ -172,6 +172,6 @@ def get_localised_doc_link():
     @returns  the link to the localised documentation if available, or to the
             english version
     """
-    return ("file:///usr/share/doc/tails/website/bug_reporting." +
+    return ("file:///usr/share/doc/tails/website/doc/first_steps/report_a_bug." +
         get_localised_doc_language() +
         ".html")
