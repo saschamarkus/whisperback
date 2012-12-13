4b9eeb80e0ee057a2a4aaba683256d633c149e29
Tails developers <amnesia@boum.org>
2012-12-13 10:58:32 +0100

    WhisperBack 1.6.4

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.6.3'
+__version__ = '1.6.4'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
