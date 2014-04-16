3f2f2388cfad7c2d113c32294c9f85e61ec8e990
Tails developers <tails@boum.org>
2014-04-16 23:26:46 +0200

    Update version to 1.6.17

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.6.16'
+__version__ = '1.6.17'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
