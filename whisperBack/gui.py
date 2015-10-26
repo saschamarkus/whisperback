c20a7706b78a7433f638afaf3045a6f6a216c015
anonym <anonym@riseup.net>
2015-10-26 18:59:25 +0100

    Update version to 1.6.31

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.6.30'
+__version__ = '1.6.31'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
