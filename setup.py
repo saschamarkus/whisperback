89ed62851503336de4690d3ff6bd6c332af3e3c1
anonym <anonym@riseup.net>
2015-11-09 17:51:32 +0100

    Update version to 1.7.1

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ build_extra.build_extra.sub_commands.insert(0, ("build_gtkbuilderi18n", None))
 build_extra.build_extra.sub_commands.append(("build_man", None))
 
 setup(name='whisperback',
-    version='1.7.0',
+    version='1.7.1',
     description='Send feedback in an encrypted mail',
     author='Tails developers',
     author_email='tails@boum.org',
