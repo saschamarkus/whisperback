cd0bc90577b9a19982cdd3ba3f2079a384fd6f06
anonym <anonym@riseup.net>
2016-02-24 17:19:25 +0100

    Update version to 1.7.4

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.7.3'
+__version__ = '1.7.4'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
