27ab41adf4e3504dfc68b2ceb8653031721c10af
anonym <anonym@riseup.net>
2017-04-06 19:41:30 +0200

    Update version to 1.7.9

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.7.8'
+__version__ = '1.7.9'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
