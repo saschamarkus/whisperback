40ffcaf7d79b5375be56a71c4864311cbd09b523
intrigeri <intrigeri@boum.org>
2018-06-02 08:59:17 +0000

    Update version to 1.7.16

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.7.15'
+__version__ = '1.7.16'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
