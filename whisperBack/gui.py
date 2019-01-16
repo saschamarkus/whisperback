5ddb15b1238553aa56cf7d1e6b7a4b7cf979f0e3
anonym <anonym@riseup.net>
2019-01-16 12:24:17 +0100

    whisperback (1.7.20)
    Gbp-Dch: Ignore

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.7.19'
+__version__ = '1.7.20'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
