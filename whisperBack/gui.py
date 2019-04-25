2e78a50f5ce66e3e100a7c0d6dc06542933a5e0c
segfault <segfault@riseup.net>
2019-04-26 01:09:08 +0200

    whisperback (1.7.22)
    Gbp-Dch: Ignore

--- whisperBack/gui.py
+++ whisperBack/gui.py
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 ########################################################################
 
-__version__ = '1.7.21'
+__version__ = '1.7.22'
 LOCALEDIR = "locale/"
 PACKAGE = "whisperback"
 
